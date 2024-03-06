023a57a3244609f1e04f11f09f4adabee740b30d
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-03-07 11:03:37 +1300

    added info in comments

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const marbleWaterInstructions = document.getElementById("marble-water-instructio
 // ====== Inputs and instructions ====== //
 function MarbleWaterInstructions(event) {
   event.preventDefault();
-  
+
   // ====== Watering ====== //
+  // If it's spring of summer and the top inches or more are dry, give instructions for how to water. If it's autumn or winter, it needs to be completely dry before watering. If values haven't been entered, ask for values.
   if (marbleSoilMoisture.value === "All dry") {
     marbleWaterInstructions.innerHTML =
       "It's time to water your pothos. Pour water into the soil until it comes out of the drainage holes, or reverse water it by putting the pot in some water and let it soak for 10 minutes to an hour.";
@@ -32,6 +33,7 @@ function MarbleWaterInstructions(event) {
   }
 
   // ====== Fertiliser ====== //
+  // If it's spring of summer, the top inches or more are dry and it's been a month or more since the last time it was fertilised, give instructions to fertilise. If it's autumn or winter or wet, no need for fertiliser. If values haven't been entered, ask for values.
   if (
     (marbleSeason.value === "Summer" || marbleSeason.value === "Spring") &&
     (marbleFertiliser.value === ">month" ||
